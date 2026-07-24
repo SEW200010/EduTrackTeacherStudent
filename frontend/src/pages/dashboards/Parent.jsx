fb1b63f8a423bcec3c417eb912f88e5480efe055
Wijesinghe Mudiyanselage Sewwandi Shashikala Wijesinghe <sewwandiwijesinghe68@gmail.com>
2026-07-24 13:34:44 +0530

    Fix backend deployment and Vercel issues

--- frontend/src/pages/dashboards/Parent.jsx
+++ frontend/src/pages/dashboards/Parent.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import API from "../api/axios";
 import Header1 from "../../components/Header1";
 import Footer1 from "../../components/Footer1";
 import API from "../../api";
