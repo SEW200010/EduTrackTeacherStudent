cf6f7432530feac7c52228e197c26d63d1de48f0
Wijesinghe Mudiyanselage Sewwandi Shashikala Wijesinghe <sewwandiwijesinghe68@gmail.com>
2026-07-24 15:44:22 +0530

    Fix missing python dependencies

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,15 +1,10 @@
-import axios from 'axios';
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
-});
-
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    baseURL: import.meta.env.VITE_API_URL,
+    headers:{
+        "Content-Type":"application/json"
+    }
 });
 
 export default API;
